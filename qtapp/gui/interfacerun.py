1b5d26499bbf5df505d47ca1740d8edbd87fb303
whunter <whunt003@odu.edu>
2017-07-19 21:06:23 -0400

    Connected T1_HorizontalSliders

--- qtapp/gui/interfacerun.py
+++ qtapp/gui/interfacerun.py
@@ -16,10 +16,9 @@ import sys
 class MplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
+        fig = Figure( dpi=dpi)
         self.axes = fig.add_subplot(111)
         # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
 
         self.compute_initial_figure()
 
@@ -36,17 +35,17 @@ class DynamicMplCanvas(MplCanvas):
     """A canvas that updates itself every second with a new plot."""
     def __init__(self, *args, **kwargs):
         MplCanvas.__init__(self, *args, **kwargs)
-        #timer = QtCore.QTimer(self)
-        #timer.timeout.connect(self.update_figure)
-        #timer.start(1000)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
 
     def compute_initial_figure(self):
         self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
 
     def update_figure(self):
         # Build a list of 4 random integers between 0 and 10 (both inclusive)
-        l = [np.random.random(0, 10) for i in range(4)]
-
+        l = np.random.uniform(0, 10, size=4)
+        self.clf()
         self.axes.plot([0, 1, 2, 3], l, 'r')
         self.draw()
 
@@ -55,11 +54,26 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('interface.ui', self)
-        self.
-        self.show()
+        #self.T1_GraphWidget_Canvas.deletelater()
+        #l = QtWidgets.QVBoxLayout(self.T1_GraphWidget_Canvas)
+        #cv = DynamicMplCanvas(self.T1_GraphWidget_Canvas, dpi=200)
+        #l.addWidget(cv)
+        self.T1_HorizontalSlider_MaxFrequency.sliderReleased.connect(self.T1_checkMaxSlider)
+        self.T1_HorizontalSlider_MinFrequency.sliderReleased.connect(self.T1_checkMinSlider)
+
+
+    def T1_checkMaxSlider(self):
+        if (self.T1_HorizontalSlider_MaxFrequency.value() <= self.T1_HorizontalSlider_MinFrequency.value()):
+            self.T1_HorizontalSlider_MinFrequency.setValue(self.T1_HorizontalSlider_MaxFrequency.value() - 1)
+
+    def T1_checkMinSlider(self):
+        if (self.T1_HorizontalSlider_MaxFrequency.value() <= self.T1_HorizontalSlider_MinFrequency.value()):
+            self.T1_HorizontalSlider_MaxFrequency.setValue(self.T1_HorizontalSlider_MinFrequency.value() + 1)
+
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
+    window.show()
     sys.exit(app.exec_())
