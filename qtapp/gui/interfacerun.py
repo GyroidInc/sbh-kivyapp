a5e0e7f10cad66ee7dc5a83445934247cdda1117
whunter <whunt003@odu.edu>
2017-07-20 08:38:02 -0400

    Further dev

--- qtapp/gui/interfacerun.py
+++ qtapp/gui/interfacerun.py
@@ -3,7 +3,7 @@
 # Example for importing a UI without compiling the .ui to a .py
 
 from PyQt5 import QtCore, QtGui,  uic, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget
+from PyQt5.QtWidgets import QApplication, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QFileDialog
 import matplotlib
 matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -21,8 +21,6 @@ class MplCanvas(FigureCanvas):
         # We want the axes cleared every time plot() is called
 
         self.compute_initial_figure()
-
-        #
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
@@ -58,17 +56,45 @@ class Ui(QtWidgets.QMainWindow):
         #l = QtWidgets.QVBoxLayout(self.T1_GraphWidget_Canvas)
         #cv = DynamicMplCanvas(self.T1_GraphWidget_Canvas, dpi=200)
         #l.addWidget(cv)
-        self.T1_HorizontalSlider_MaxFrequency.sliderReleased.connect(self.T1_checkMaxSlider)
-        self.T1_HorizontalSlider_MinFrequency.sliderReleased.connect(self.T1_checkMinSlider)
+        #self.T1_HorizontalSlider_MaxFrequency.sliderReleased.connect(self.T1_checkMaxSlider)
+        #self.T1_HorizontalSlider_MinFrequency.sliderReleased.connect(self.T1_checkMinSlider)
+        self.T1_SpinBox_MinFrequency.valueChanged.connect(self.T1_checkMinSlider)
+        self.T1_SpinBox_MaxFrequency.valueChanged.connect(self.T1_checkMaxSlider)
+        self.T1_Button_LoadDirectory.clicked.connect(self.openFileNamesDialog)
+
+
+
 
 
     def T1_checkMaxSlider(self):
         if (self.T1_HorizontalSlider_MaxFrequency.value() <= self.T1_HorizontalSlider_MinFrequency.value()):
-            self.T1_HorizontalSlider_MinFrequency.setValue(self.T1_HorizontalSlider_MaxFrequency.value() - 1)
+            toSet= self.T1_HorizontalSlider_MaxFrequency.value() - 1
+            if toSet < 0:
+                toSet=0
+                self.T1_HorizontalSlider_MaxFrequency.setValue(1)
+
+            self.T1_HorizontalSlider_MinFrequency.setValue(toSet)
 
     def T1_checkMinSlider(self):
         if (self.T1_HorizontalSlider_MaxFrequency.value() <= self.T1_HorizontalSlider_MinFrequency.value()):
-            self.T1_HorizontalSlider_MaxFrequency.setValue(self.T1_HorizontalSlider_MinFrequency.value() + 1)
+            toSet = self.T1_HorizontalSlider_MinFrequency.value() + 1
+            #TODO fix this so that it doesn't crash
+            #if toSet >= self.T1_SpinBox_MaxFrequency.maximum:
+            #    toSet = self.T1_SpinBox_MaxFrequency.maximum
+            #    self.T1_HorizontalSlider_MinFrequency.setValue(toSet - 1)
+
+            self.T1_HorizontalSlider_MaxFrequency.setValue(toSet)
+
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "",
+                                                "All Files (*);;excel files (*.xlsx)", options=options)
+        if files:
+            #TODO do something with selected files
+            print(files)
+
+
 
 
 
