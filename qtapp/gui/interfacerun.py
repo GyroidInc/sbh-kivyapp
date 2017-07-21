1cf2424a323964e2b2d50522986bdfef383930d1
whunter <whunt003@odu.edu>
2017-07-21 00:35:18 -0400

    ComboBox changes the file loading window now

--- qtapp/gui/interfacerun.py
+++ qtapp/gui/interfacerun.py
@@ -12,6 +12,7 @@ from matplotlib.figure import Figure
 import numpy as np
 
 import sys
+import os
 
 
 
@@ -20,23 +21,25 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         uic.loadUi('interface2.ui', self)
 
-
+        #setting up matplotlib element
         self.hbox = QtWidgets.QVBoxLayout()
         self.MplCanvas = DynamicMplCanvas()
         self.hbox.addWidget(self.MplCanvas)
         self.T1_GraphWindow.setLayout(self.hbox)
+
+        # TODO graph doesnt update on slider button press/change
+        # connecting graph refresh upon slider release
         self.T1_HorizontalSlider_MaxFrequency.sliderReleased.connect(self.MplCanvas.update_figure)
         self.T1_HorizontalSlider_MinFrequency.sliderReleased.connect(self.MplCanvas.update_figure)
 
+
+
+        # adding slider behavior checks
         self.T1_SpinBox_MinFrequency.valueChanged.connect(self.T1_checkMinSlider)
         self.T1_SpinBox_MaxFrequency.valueChanged.connect(self.T1_checkMaxSlider)
-        self.T1_Button_LoadDirectory.clicked.connect(self.openFileNamesDialog)
-
-    def replaceGfx(self):
-
-        #self.title1.deleteLater()
-        #self.gridLayout.addWidget(self.title2, 1, 5)
-        pass
+        self.T1_Button_LoadDirectory.clicked.connect(self.openFolderNamesDialog)
+        # set load file by behavior
+        self.T1_ComboBox_LabelFilesBy.currentIndexChanged.connect(self.selectLoadFileType)
 
 
 
@@ -60,15 +63,35 @@ class Ui(QtWidgets.QMainWindow):
 
             self.T1_HorizontalSlider_MaxFrequency.setValue(toSet)
 
+    def selectLoadFileType(self):
+        try:
+            self.T1_Button_LoadDirectory.clicked.disconnect()
+        except Exception:
+            print("button disconnect issue")
+
+        if (self.T1_ComboBox_LabelFilesBy.currentText() == "CSV File"):
+            self.T1_Button_LoadDirectory.clicked.connect(self.openFileNamesDialog)
+        else:
+            self.T1_Button_LoadDirectory.clicked.connect(self.openFolderNamesDialog)
+
+
     def openFileNamesDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "",
+        files, _ = QFileDialog.getOpenFileNames(self,"Load : Label by File", "",
                                                 "All Files (*);;excel files (*.xlsx)", options=options)
         if files:
             #TODO do something with selected files
             print(files)
 
+    def openFolderNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontResolveSymlinks | QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog
+        directory = QFileDialog.getExistingDirectory(self,
+                    "Load : Label By Folder", os.path.expanduser("~"), options=options)
+        if directory:
+            #TODO do something with selected files
+            print (directory)
 
 
 
